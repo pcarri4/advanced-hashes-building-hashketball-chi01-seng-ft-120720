e62827607362be6e2fc705fd6c1be3c7ede17f5b
Paulina Carrillo <paulinacarri4@gmail.com>
2020-11-29 22:47:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -76,46 +76,46 @@ def game_hash
         },
         {:player_name => "Bismack Biyombo"
         :number => 0
-        :shoe => 
-        :points => 
-        :rebounds => 
-        :assists => 
-        :steals => 
-        :blocks => 
-        :slam_dunks =>
+        :shoe => 16
+        :points => 12
+        :rebounds => 4
+        :assists => 7
+        :steals => 22
+        :blocks => 15
+        :slam_dunks => 10
           
         },
         {:player_name => "DeSagna Diop"
-        :number => 
-        :shoe => 
-        :points => 
-        :rebounds => 
-        :assists => 
-        :steals => 
-        :blocks => 
-        :slam_dunks =>
+        :number => 2 
+        :shoe => 14
+        :points => 24
+        :rebounds => 12
+        :assists => 12
+        :steals => 4 
+        :blocks => 5 
+        :slam_dunks => 5
           
         },
         {:player_name => "Ben Gordon"
-        :number => 
-        :shoe => 
-        :points => 
-        :rebounds => 
-        :assists => 
-        :steals => 
-        :blocks => 
-        :slam_dunks =>
+        :number => 8 
+        :shoe => 15 
+        :points => 33 
+        :rebounds => 3 
+        :assists => 2 
+        :steals => 1 
+        :blocks => 1 
+        :slam_dunks => 0
           
         },
         {:player_name => "Kemba Walker"
-        :number => 
-        :shoe => 
-        :points => 
-        :rebounds => 
-        :assists => 
-        :steals => 
-        :blocks => 
-        :slam_dunks =>
+        :number => 33
+        :shoe => 15
+        :points => 6 
+        :rebounds => 12
+        :assists => 12
+        :steals => 7 
+        :blocks => 5 
+        :slam_dunks => 12
           
         }]
     }
